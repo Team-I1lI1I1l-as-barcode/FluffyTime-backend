ff6c944fdc6ed05645fc6df1c621bb207f6c2574
kwonkeonhyeong <rjsgud19566@gmail.com>
2024-07-31 13:56:00 +0900

    create project

---
+++ src/main/java/com/fluffytime/FluffytimeApplication.java
@@ -0,0 +1,13 @@
+package com.fluffytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffytimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluffytimeApplication.class, args);
+    }
+
+}
