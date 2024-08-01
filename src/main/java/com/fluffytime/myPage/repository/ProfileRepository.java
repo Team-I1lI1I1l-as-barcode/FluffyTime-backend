81b2b12dc40c25e808048011041f55dbaa35f751
Yoong <52689951+Yoong-D@users.noreply.github.com>
2024-08-02 04:23:29 +0900

    Revert "[feature/마이페이지-마이페이지-화면] 마이페이지 화면 구현"

--- src/main/java/com/fluffytime/myPage/repository/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fluffytime.myPage.repository;
-
-import com.fluffytime.domain.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
-}
