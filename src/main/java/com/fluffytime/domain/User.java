e0d2258f7b85c9d84da1dcbefdd8d18c8efdefe0
yooong-D <as2132as@naver.com>
2024-08-02 04:04:40 +0900

    마이페이지 화면 구성 완료
    마이페이지의 화면의 기본적인 틀을 구성했습니다.
    마이페이지에서 api를 통해 사용자 정보를 불러와 마이페이지에 접속했을시
    나의 관련 정보가 화면에 출력됩니다.
    
    1. 팔로우, 팔로워 기능과 합치게 된다면 이에 대한 정보 출력 필요
    2. 이미지 업로드 기능 구현 전이라 게시물 부분엔 게시물 제목이 나타나도록 설정

--- src/main/java/com/fluffytime/domain/User.java
+++ src/main/java/com/fluffytime/domain/User.java
@@ -3,10 +3,13 @@ package com.fluffytime.domain;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
@@ -42,6 +45,11 @@ public class User {
         orphanRemoval = true
     )
     private List<Post> postList = new ArrayList<>();
+
+    // 유저 - 프로필 (일대일 단방향)
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "profile_id")
+    private Profile profile;
 
     @PrePersist
     public void create() {
