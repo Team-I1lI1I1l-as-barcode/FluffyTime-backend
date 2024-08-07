bedf80a0ab6313fe27aa37239001bf91852902be
kongkong2233 <dbqls1703@naver.com>
2024-08-08 01:41:08 +0900

    feat: 답글 등록 및 삭제 + 수정 기능 생성
    답글 등록 및 삭제, 수정 기능 생성 완료
    등록/수정 js 수정 예정

---
+++ src/main/java/com/fluffytime/reply/service/ReplyService.java
@@ -0,0 +1,63 @@
+package com.fluffytime.reply.service;
+
+import com.fluffytime.comment.config.error.exception.PostNotFoundException;
+import com.fluffytime.comment.config.error.exception.UserNotFoundException;
+import com.fluffytime.domain.Comment;
+import com.fluffytime.domain.Reply;
+import com.fluffytime.domain.User;
+import com.fluffytime.reply.dto.ReplyRequestDto;
+import com.fluffytime.reply.dto.ReplyResponseDto;
+import com.fluffytime.repository.CommentRepository;
+import com.fluffytime.repository.ReplyRepository;
+import com.fluffytime.repository.UserRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReplyService {
+
+    private final ReplyRepository replyRepository;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+
+    //답글 저장
+    public void createReply(ReplyRequestDto requestDto) {
+        User user = userRepository.findById(requestDto.getUserId())
+            .orElseThrow(UserNotFoundException::new);
+        Comment comment = commentRepository.findById(requestDto.getCommentId())
+            .orElseThrow(PostNotFoundException::new); //임시 예외처리
+
+        Reply reply = Reply.builder()
+            .content(requestDto.getContent())
+            .user(user)
+            .comment(comment)
+            .build();
+        replyRepository.save(reply);
+    }
+
+    //답글 조회
+    public List<ReplyResponseDto> getRepliesByCommentId(Long commentId) {
+        List<Reply> replyList = replyRepository.findByCommentCommentId(commentId);
+        return replyList.stream()
+            .map(ReplyResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
+    //답글 수정
+    public void updateReply(Long replyId, String content) {
+        Reply reply = replyRepository.findById(replyId)
+            .orElseThrow(() -> new RuntimeException("reply not found"));
+        reply.setContent(content);
+        replyRepository.save(reply);
+    }
+
+    //답글 삭제
+    public void deleteReply(Long replyId) {
+        Reply reply = replyRepository.findById(replyId)
+            .orElseThrow(() -> new RuntimeException("reply not found"));
+        replyRepository.delete(reply);
+    }
+}
