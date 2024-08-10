e0f107cba41bbbf82b4a8720ad3bb95e552c1611
yooong-D <as2132as@naver.com>
2024-08-10 17:14:31 +0900

    feat : 마이페이지 화면 디자인
    마이페이지 화면을 디자인합니다. (HTML,CSS 수정)
    
    - **게시물이 있을 때**
      - 게시물 나열을 위한 CSS 구현
      - 게시물 표시 로직을 수정하는 JavaScript 추가
    
    - **게시물이 없을 때**
      - 게시물 추가를 유도하는 CSS 구현
      - 게시물 추가 로직을 수정하는 JavaScript 추가
    
    - **프로필 사진 관련**
      - 마이페이지에서 프로필 사진 클릭 시 사진 업데이트 기능 제공
      - 프로필 사진에 블러 효과 적용
      - 프로필 사진에 카메라 아이콘 추가
      - 프로필 사진 영역 클릭 시 사진 업데이트 기능 활성화

--- src/main/resources/static/js/mypage/mypage.js
+++ src/main/resources/static/js/mypage/mypage.js
@@ -9,6 +9,7 @@ const pet_sex = getElement("pet_sex"); // 반려동물 성별
 const pet_age = getElement("pet_age"); // 반려동물 나이
 const intro = getElement("intro"); // 소개글
 const img = getElement('img'); // 이미지 미리보기
+const imagePreview = getElement('imagePreview'); // 프로필 이미지 영역
 // const follower_count = getElement("follower_count"); // 팔로워 수
 // const follow_count = getElement("follow_count");// 팔로우 수
 
@@ -39,12 +40,46 @@ function fetchMyPage(url, func) {
   });
 }
 
+// 사진 등록/수정 api 요청 함수
+function fetchProfileImage(method, url) {
+  const profileForm = document.getElementById('profileImageForm');
+  const formData = new FormData(profileForm);
+  fetch(url, {
+    method: method,
+    body: formData
+  })
+  .then(response => {
+    if (!response.ok) {
+      return response.json().then(errorData => {
+        console.error(errorData.message);
+        window.location.href = "/";
+        throw new Error(errorData.message);
+      });
+    }
+    return response.json();
+  })
+  .then(data => {
+    if (data.result) {
+      alert("사진이 정상적으로 등록되었습니다.");
+      img.src = data.fileUrl; // img의 src 속성을 서버에서 반환된 URL로 업데이트
+      window.location.reload(); // 새로 고침
+    } else {
+      alert("사진 등록에 실패하였습니다.");
+    }
+  })
+  .catch(error => console.error("서버 오류 발생: " + error));
+}
+
 // 마이페이지 정보 로드 함수
 function handleProfileData(data) {
   // 성공적인 응답 시
   console.log("fetchMyPage 응답 Success");
   nickName.innerText = data.nickname;
-  posts_count.innerText = data.postsList.length;
+  if (data.postsList === null) {
+    posts_count.innerText = 0;
+  } else {
+    posts_count.innerText = data.postsList.length;
+  }
   pet_name.innerText = data.petName;
   if (data.petSex === "none") {
     pet_sex.innerText = " ";
@@ -72,14 +107,21 @@ function myPageEdit(data) {
 // 게시물 목록을 렌더링하는 함수
 function renderPosts(posts) {
   console.log("renderPosts 실행");
-  const postListElement = getElement('post_list');
-  postListElement.innerHTML = ''; // 기존 게시물 리스트 비우기
+  const postListElement = document.querySelector('#post_list');
+  if (posts !== null) {
+    // 기존 자식 요소들을 하나씩 제거
+    while (postListElement.firstChild) {
+      postListElement.removeChild(postListElement.firstChild);
+    }
 
-  posts.forEach(post => {
-    const li = document.createElement('li');
-    li.textContent = post.title; // 게시물 제목 추가
-    postListElement.appendChild(li);
-  });
+    posts.forEach(post => {
+      const img = document.createElement('img'); // <img> 요소 생성
+      img.src = post.imageUrl; // 이미지 URL 설정
+      img.alt = post.title; // 이미지 설명 설정
+
+      postListElement.appendChild(img); // <img>를 섹션에 추가
+    });
+  }
 }
 
 // 프로필 편집 버튼 설정 함수
@@ -93,8 +135,36 @@ function setupProfileEditButton() {
 // 초기화 함수
 function initialize() {
   console.log("initialize 실행");
+  const profileForm = document.getElementById('profileImageForm');
+  const nickname = window.location.pathname.split('/').pop();
+
   setupProfileEditButton();
   fetchMyPage("/api/mypage/info", handleProfileData); // 마이페이지 정보 불러오기
+
+  // 초기화 - 프로필 사진 클릭시 파일 선택 버튼이 눌림
+  imagePreview.addEventListener('click', event => {
+    event.preventDefault();
+    // 프로필 파일이 없을 시 사진 파일 선택창 열기
+    document.getElementById("images").click();
+  });
+
+  // 초기화 - 프로필 사진 등록
+  document.getElementById("images").addEventListener('change', event => {
+    event.preventDefault();
+
+    // 기본 이미지일 경우 이미지 등록 api 요청
+    if (img.src === "../../../image/profile/profile.png") {
+      console.log("프로필 사진 등록 api 요청")
+      fetchProfileImage('POST',
+          `/api/mypage/profiles/images/reg?nickname=${encodeURIComponent(
+              nickname)}`);
+    } else { // 아닐 경우 이미지 업데이트 api 요청
+      console.log("프로필 사진 업데이트 api 요청")
+      fetchProfileImage('PATCH',
+          `/api/mypage/profiles/images/edit?nickname=${encodeURIComponent(
+              nickname)}`);
+    }
+  });
 }
 
 // 페이지 로드 시 초기화 함수 호출
