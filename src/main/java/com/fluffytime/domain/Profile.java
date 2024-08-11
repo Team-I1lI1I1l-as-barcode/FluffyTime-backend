a4dd056c14e5f0ebd613ea7c6fe528e6ba9f89db
chanwoo <cw2581@naver.com>
2024-08-12 01:20:15 +0900

    feat: 태그 검색을 제외한 검색기능 구현
    db연결을 통해 유저와 프로필정보를 불러와 검색할 수 있다. 무한스크롤, 태그 검색, css 미구현.
    user, profile 엔티티 사이의 관계를 단방향에서 양방향으로 전환했음

--- src/main/java/com/fluffytime/domain/Profile.java
+++ src/main/java/com/fluffytime/domain/Profile.java
@@ -2,9 +2,12 @@ package com.fluffytime.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -44,6 +47,10 @@ public class Profile {
 
     @Column(name = "public_status", nullable = false, length = 10)
     private String publicStatus;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @PrePersist
     public void create() {
