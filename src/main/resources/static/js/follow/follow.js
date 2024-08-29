a3b744c38865c70690b1ff5541fec7508cff3fef
chanwoo <cw2581@naver.com>
2024-08-29 16:46:07 +0900

    ♻️ 팔로우/팔로잉 목록 로직 중복코드 재사용하게 리팩토링

--- src/main/resources/static/js/follow/follow.js
+++ src/main/resources/static/js/follow/follow.js
@@ -147,12 +147,152 @@ document.querySelectorAll('.follow-close').forEach(closeButton => {
     }
   };
 });
-
-// 팔로워 리스트 API 호출 함수
-async function fetchFollowerList(nickname) {
-  console.log("팔로워 리스트 api 호출");
+//
+// // 팔로워 리스트 API 호출 함수
+// async function fetchFollowerList(nickname) {
+//   console.log("팔로워 리스트 api 호출");
+//   try {
+//     const response = await fetch(`/api/follow/search/followers/${nickname}`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json"
+//       }
+//     });
+//
+//     if (!response.ok) {
+//       throw new Error('팔로워 목록을 가져오는 중 오류 발생');
+//     }
+//
+//     const followers = await response.json();
+//     const followerListElement = document.getElementById('followerList');
+//     followerListElement.innerHTML = ''; // 기존 목록 초기화
+//
+//     followers.forEach(follower => {
+//       const li = document.createElement('li');
+//       li.className = 'follower-item'; // 클래스 이름 추가
+//
+//       const linkBox = document.createElement('div');
+//       linkBox.className = 'linkBox';
+//
+//       const img = document.createElement('img');
+//       img.src = follower.profileImageUrl; // 프로필 이미지 URL 설정
+//       img.alt = `${follower.nickname}의 프로필 이미지`; // 이미지 대체 텍스트 설정
+//       img.className = 'follower-profile-image'; // 프로필 이미지에 대한 클래스 이름 설정
+//
+//       const details = document.createElement('div');
+//       details.className = 'details';
+//
+//       const nickname = document.createElement('div');
+//       nickname.className = 'nickname';
+//       nickname.textContent = follower.nickname; // 팔로워 닉네임 표시
+//
+//       const intro = document.createElement('div');
+//       intro.className = 'intro';
+//       intro.textContent = follower.intro; // 팔로워의 한줄 소개 표시
+//
+//       // "자기 소개 없음"이면 글씨 회색으로
+//       if (intro.textContent === "자기 소개 없음") {
+//         intro.style.color = '#A9A9A9';
+//       }
+//
+//       details.appendChild(nickname);
+//       details.appendChild(intro);
+//
+//       linkBox.appendChild(img);
+//       linkBox.appendChild(details);
+//
+//       //클릭 시 유저 페이지로 이동
+//       linkBox.addEventListener('click', () => {
+//         window.location.href = `/userpages/${follower.nickname}`;
+//       });
+//
+//       li.appendChild(linkBox);
+//
+//       // 리스트 항목을 팔로워 목록에 추가
+//       followerListElement.appendChild(li);
+//     });
+//
+//     const modal = document.getElementById('followerList-modal');
+//     openModal(modal);
+//   } catch (error) {
+//     console.error('fetchFollowerList() Error:', error);
+//   }
+// }
+//
+// // 팔로잉 리스트 API 호출 함수
+// async function fetchFollowingList(nickname) {
+//   console.log("팔로잉 리스트 api 호출");
+//   try {
+//     const response = await fetch(`/api/follow/search/followings/${nickname}`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json"
+//       }
+//     });
+//
+//     if (!response.ok) {
+//       throw new Error('팔로잉 목록을 가져오는 중 오류 발생');
+//     }
+//
+//     const followings = await response.json();
+//     const followingListElement = document.getElementById('followingList');
+//     followingListElement.innerHTML = ''; // 기존 목록 초기화
+//
+//     followings.forEach(following => {
+//       const li = document.createElement('li');
+//       li.className = 'following-item'; // 클래스 이름 추가
+//
+//       const linkBox = document.createElement('div');
+//       linkBox.className = 'linkBox';
+//
+//       const img = document.createElement('img');
+//       img.src = following.profileImageUrl; // 프로필 이미지 URL 설정
+//       img.alt = `${following.nickname}의 프로필 이미지`; // 이미지 대체 텍스트 설정
+//       img.className = 'following-profile-image'; // 프로필 이미지에 대한 클래스 이름 설정
+//
+//       const details = document.createElement('div');
+//       details.className = 'details';
+//
+//       const nickname = document.createElement('div');
+//       nickname.className = 'nickname';
+//       nickname.textContent = following.nickname; // 팔로워 닉네임 표시
+//
+//       const intro = document.createElement('div');
+//       intro.className = 'intro';
+//       intro.textContent = following.intro; // 팔로워의 한줄 소개 표시
+//
+//       // "자기 소개 없음"이면 글씨 회색으로
+//       if (intro.textContent === "자기 소개 없음") {
+//         intro.style.color = '#A9A9A9';
+//       }
+//
+//       details.appendChild(nickname);
+//       details.appendChild(intro);
+//
+//       linkBox.appendChild(img);
+//       linkBox.appendChild(details);
+//
+//       //클릭 시 유저 페이지로 이동
+//       linkBox.addEventListener('click', () => {
+//         window.location.href = `/userpages/${following.nickname}`;
+//       });
+//
+//       li.appendChild(linkBox);
+//
+//       // 리스트 항목을 팔로워 목록에 추가
+//       followingListElement.appendChild(li);
+//     });
+//
+//     const modal = document.getElementById('followingList-modal');
+//     openModal(modal);
+//   } catch (error) {
+//     console.error('fetchFollowingList() Error:', error);
+//   }
+// }
+async function fetchList(type, nickname) {
+  console.log(`${type} 리스트 API 호출`);
   try {
-    const response = await fetch(`/api/follow/search/followers/${nickname}`, {
+    const response = await fetch(`/api/follow/search/${type}s/${nickname}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json"
@@ -160,134 +300,74 @@ async function fetchFollowerList(nickname) {
     });
 
     if (!response.ok) {
-      throw new Error('팔로워 목록을 가져오는 중 오류 발생');
+      throw new Error(`${type} 목록을 가져오는 중 오류 발생`);
     }
 
-    const followers = await response.json();
-    const followerListElement = document.getElementById('followerList');
-    followerListElement.innerHTML = ''; // 기존 목록 초기화
+    const items = await response.json();
+    const listElement = document.getElementById(`${type}List`);
+    listElement.innerHTML = ''; // 기존 목록 초기화
 
-    followers.forEach(follower => {
+    items.forEach(item => {
       const li = document.createElement('li');
-      li.className = 'follower-item'; // 클래스 이름 추가
+      li.className = `${type}-item`; // 클래스 이름 추가
 
       const linkBox = document.createElement('div');
       linkBox.className = 'linkBox';
 
       const img = document.createElement('img');
-      img.src = follower.profileImageUrl; // 프로필 이미지 URL 설정
-      img.alt = `${follower.nickname}의 프로필 이미지`; // 이미지 대체 텍스트 설정
-      img.className = 'follower-profile-image'; // 프로필 이미지에 대한 클래스 이름 설정
+      img.src = item.profileImageUrl; // 프로필 이미지 URL 설정
+      img.alt = `${item.nickname}의 프로필 이미지`; // 이미지 대체 텍스트 설정
+      img.className = `${type}-profile-image`; // 프로필 이미지에 대한 클래스 이름 설정
 
       const details = document.createElement('div');
       details.className = 'details';
 
-      const nickname = document.createElement('div');
-      nickname.className = 'nickname';
-      nickname.textContent = follower.nickname; // 팔로워 닉네임 표시
+      const nicknameDiv = document.createElement('div');
+      nicknameDiv.className = 'nickname';
+      nicknameDiv.textContent = item.nickname; // 팔로워/팔로잉 닉네임 표시
 
       const intro = document.createElement('div');
       intro.className = 'intro';
-      intro.textContent = follower.intro; // 팔로워의 한줄 소개 표시
+      intro.textContent = item.intro; // 팔로워/팔로잉의 한줄 소개 표시
 
       // "자기 소개 없음"이면 글씨 회색으로
       if (intro.textContent === "자기 소개 없음") {
         intro.style.color = '#A9A9A9';
       }
 
-      details.appendChild(nickname);
+      details.appendChild(nicknameDiv);
       details.appendChild(intro);
 
       linkBox.appendChild(img);
       linkBox.appendChild(details);
 
-      //클릭 시 유저 페이지로 이동
+      // 클릭 시 유저 페이지로 이동
       linkBox.addEventListener('click', () => {
-        window.location.href = `/userpages/${follower.nickname}`;
+        window.location.href = `/userpages/${item.nickname}`;
       });
 
       li.appendChild(linkBox);
 
-      // 리스트 항목을 팔로워 목록에 추가
-      followerListElement.appendChild(li);
+      // 리스트 항목을 리스트에 추가
+      listElement.appendChild(li);
     });
 
-    const modal = document.getElementById('followerList-modal');
+    const modal = document.getElementById(`${type}List-modal`);
     openModal(modal);
   } catch (error) {
-    console.error('fetchFollowerList() Error:', error);
+    console.error(
+        `fetch ${type}List() Error:`, error);
   }
+}
+
+// 팔로워 리스트 API 호출 함수
+async function fetchFollowerList(nickname) {
+  await fetchList('follower', nickname);
 }
 
 // 팔로잉 리스트 API 호출 함수
 async function fetchFollowingList(nickname) {
-  console.log("팔로잉 리스트 api 호출");
-  try {
-    const response = await fetch(`/api/follow/search/followings/${nickname}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error('팔로잉 목록을 가져오는 중 오류 발생');
-    }
-
-    const followings = await response.json();
-    const followingListElement = document.getElementById('followingList');
-    followingListElement.innerHTML = ''; // 기존 목록 초기화
-
-    followings.forEach(following => {
-      const li = document.createElement('li');
-      li.className = 'following-item'; // 클래스 이름 추가
-
-      const linkBox = document.createElement('div');
-      linkBox.className = 'linkBox';
-
-      const img = document.createElement('img');
-      img.src = following.profileImageUrl; // 프로필 이미지 URL 설정
-      img.alt = `${following.nickname}의 프로필 이미지`; // 이미지 대체 텍스트 설정
-      img.className = 'following-profile-image'; // 프로필 이미지에 대한 클래스 이름 설정
-
-      const details = document.createElement('div');
-      details.className = 'details';
-
-      const nickname = document.createElement('div');
-      nickname.className = 'nickname';
-      nickname.textContent = following.nickname; // 팔로워 닉네임 표시
-
-      const intro = document.createElement('div');
-      intro.className = 'intro';
-      intro.textContent = following.intro; // 팔로워의 한줄 소개 표시
-
-      // "자기 소개 없음"이면 글씨 회색으로
-      if (intro.textContent === "자기 소개 없음") {
-        intro.style.color = '#A9A9A9';
-      }
-
-      details.appendChild(nickname);
-      details.appendChild(intro);
-
-      linkBox.appendChild(img);
-      linkBox.appendChild(details);
-
-      //클릭 시 유저 페이지로 이동
-      linkBox.addEventListener('click', () => {
-        window.location.href = `/userpages/${following.nickname}`;
-      });
-
-      li.appendChild(linkBox);
-
-      // 리스트 항목을 팔로워 목록에 추가
-      followingListElement.appendChild(li);
-    });
-
-    const modal = document.getElementById('followingList-modal');
-    openModal(modal);
-  } catch (error) {
-    console.error('fetchFollowingList() Error:', error);
-  }
+  await fetchList('following', nickname);
 }
 
 // 페이지 로드 시 이벤트 핸들러 설정
